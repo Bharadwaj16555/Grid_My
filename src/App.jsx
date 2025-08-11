88edc49130c1fcbd9cf8e36590c6146bdc03c416
Bharadwaj16555 <bharadwajrhl@gmail.com>
2025-08-11 11:55:43 +0530

    My

--- src/App.jsx
+++ src/App.jsx
@@ -18,32 +18,32 @@ class App extends Component {
         <section>
           {/* Each card displays an image and a label/title below it */}
           <div className='card'>
-            <img src='/1.jpeg' alt='' />
+            <img src='/1.jpeg' alt='image 1' />
             <label>Project One</label>
           </div>
 
           <div className='card'>
-            <img src='/2.jpeg' alt='' />
+            <img src='/2.jpeg' alt='image 2' />
             <label>Project Two</label>
           </div>
 
           <div className='card'>
-            <img src='/3.jpeg' alt='' />
+            <img src='/3.jpeg' alt='image 3' />
             <label>Project Three</label>
           </div>
 
           <div className='card'>
-            <img src='/4.jpeg' alt='' />
+            <img src='/4.jpeg' alt='image 4' />
             <label>Project Four</label>
           </div>
 
           <div className='card'>
-            <img src='/5.jpeg' alt='' />
+            <img src='/5.jpeg' alt='image 5' />
             <label>Project Five</label>
           </div>
 
           <div className='card'>
-            <img src='/6.jpeg' alt='' />
+            <img src='/6.jpeg' alt='image 6' />
             <label>Project Six</label>
           </div>
         </section>
